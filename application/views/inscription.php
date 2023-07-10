2c147799a990d41981e88c7ba4e043cda0792975
Mananjara1754 <mananjara888@gmail.com>
2023-07-10 14:23:02 +0300

    Inscription interface 2

--- application/views/inscription.php
+++ application/views/inscription.php
@@ -8,19 +8,17 @@
     <meta name="generator" content="Hugo 0.104.2">
     <title>Signin</title>
 
-
     <link href="<?php echo base_url('assets/dist/css/bootstrap.min.css'); ?>" rel="stylesheet">
 
     <link href="<?php echo base_url('assets/style/signin.css'); ?>" rel="stylesheet">
-    <!-- Custom styles for this template -->
+
     <style>
         #formulaireInscription{
-            /* border: double; */
             max-width: 350px;
             padding: 6% 2%;
             border-radius:  43px;
             box-shadow:  21px 21px 38px #f3eeee,
-             -21px -21px 38px #f7f2f2;
+            -21px -21px 38px #f7f2f2;
         }
         body{
             background-image: url(assets/img/2.png);
@@ -35,43 +33,45 @@
     <h1 class="h3 mb-3 fw-normal">Inscription</h1>
 
     <div class="form-floating">
-      <input id="nom" type="email" class="form-control" placeholder="Nom">
+      <input name="nom" class="form-control" placeholder="Nom">
       <label for="floatingPassword">Nom</label>
     </div><br>
     <div class="row">
         <div class="col-md-6">
             <div class="form-floating">
-            <input id="nom" type="email" class="form-control" placeholder="Nom">
+            <input name="age" class="form-control" placeholder="Age">
             <label for="floatingPassword">Age</label>
             </div><br>
         </div>
         <div class="col-md-6">
             <div class="form-floating">
-            <input id="nom" type="email" class="form-control" placeholder="Nom">
-            <label for="floatingPassword">Sexe</label>
+                <select name="idgenre" id="" class="form-control">
+                    <option>Sexe</option>
+                    <option value="">Male</option>
+                </select>
             </div><br>
         </div>
     </div>
 
     <div class="form-floating">
-      <input id="nom" type="email" class="form-control" placeholder="Nom">
+      <input id="nom" type="email" name="nemail" class="form-control" placeholder="Email">
       <label for="floatingPassword">Email</label>
     </div><br>
     
     <div class="form-floating">
-      <input id="nom" type="email" class="form-control" placeholder="Nom">
+      <input name="mdp" type="password" class="form-control" placeholder="Mots de passe">
       <label for="floatingPassword">Mots de Passe</label>
     </div><br>
     <div class="row">
         <div class="col-md-6">
             <div class="form-floating">
-            <input id="nom" type="email" class="form-control" placeholder="Nom">
+            <input name="poids" id="nom" type="email" class="form-control" placeholder="Poids">
             <label for="floatingPassword">Poids</label>
             </div><br>
         </div>
         <div class="col-md-6">
             <div class="form-floating">
-            <input id="nom" type="email" class="form-control" placeholder="Nom">
+            <input name="taille" type="email" class="form-control" placeholder="Taille">
             <label for="floatingPassword">Taille</label>
             </div><br>
         </div>
